4a75a6cb3673c359384e80fcea12ba2d59908f6c
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-12-08 11:27:12 +0530

    separate index and app

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,11 @@
-import express from "express";
 import dotenv from "dotenv";
+import app from "./app.js";
 
 dotenv.config({
   path: "./.env",
 });
 
-const app = express();
-
 const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello from test!");
-});
 
 app.listen(PORT, () => {
   console.log(`Server listening to port ${PORT}`);
