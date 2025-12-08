31e717fb2e8722707469f88832a724b5b59bf6d8
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-12-08 11:22:35 +0530

    setup express

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,18 @@
-console.log("hi");
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from test!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening to port ${PORT}`);
+});
