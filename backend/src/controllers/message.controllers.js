2686376428890e39b9bb15a71c5b1a41e80721fd
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-12-11 11:21:06 +0530

    create get all contacts controller

--- backend/src/controllers/message.controllers.js
+++ backend/src/controllers/message.controllers.js
@@ -2,3 +2,23 @@ import { asyncHandler } from "../utils/async-handler.js";
 import { ApiResponse } from "../utils/api-response.js";
 import { ApiError } from "../utils/api-error.js";
 import { Message } from "../models/message.model.js";
+import { User } from "../models/user.model.js";
+
+export const getAllContacts = asyncHandler(async (req, res) => {
+  const loggedInUserId = req.user._id;
+  const filteredUsers = await User.find({
+    _id: { $ne: loggedInUserId },
+  }).select("-password -refreshToken");
+
+  if (!filteredUsers) throw new ApiError(400, "Failed to get all contacts!");
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { filteredUsers },
+        "Fetched all users successfully!",
+      ),
+    );
+});
--- backend/src/routes/message.routes.js
+++ backend/src/routes/message.routes.js
@@ -1,5 +1,13 @@
 import { Router } from "express";
+import { getAllContacts } from "../controllers/message.controllers.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
+
+router.route("/contacts").get(verifyJWT, getAllContacts);
+// router.route("/chats").get(getChatParteners);
+// router.route("/get-message/:id").get(getMessageByUserId);
+
+// router.route("/send-message/:id").post(sendMessagetoUser);
 
 export default router;
