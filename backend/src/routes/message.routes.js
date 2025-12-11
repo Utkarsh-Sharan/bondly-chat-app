211409fe8f520d9888b5dd7a9727166a38b4b5bf
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-12-11 12:23:21 +0530

    use rate limiter in message routes

--- backend/src/routes/message.routes.js
+++ backend/src/routes/message.routes.js
@@ -6,9 +6,14 @@ import {
   getChatPartners,
 } from "../controllers/message.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { arcjetProtection } from "../middlewares/arcjet.middleware.js";
 
 const router = Router();
 
+//Rate limiter/ Bots protection
+router.use(arcjetProtection);
+
+//Protected routes
 router.route("/contacts").get(verifyJWT, getAllContacts);
 router.route("/chats").get(verifyJWT, getChatPartners);
 router.route("/get-messages/:id").get(verifyJWT, getMessagesByUserId);
