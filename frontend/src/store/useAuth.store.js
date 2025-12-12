d603005ef0d2eabdd646c79bf5b9bd9be733aa06
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-12-12 11:53:24 +0530

    update use auth store

--- frontend/src/store/useAuth.store.js
+++ frontend/src/store/useAuth.store.js
@@ -1,10 +1,19 @@
 import { create } from "zustand";
+import { axiosInstance } from "../lib/axios.js";
 
 export const useAuthStore = create((set) => ({
-  authUser: { name: "john", _id: 123, age: 25 },
-  isLoggedIn: false,
-  login: () => {
-    console.log("Just logged in!");
-    set({ isLoggedIn: true });
+  authUser: null,
+  isCheckingAuth: true,
+
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/login");
+      set({ authUser: res.data });
+    } catch (error) {
+      console.log(`Error in auth check ${error}`);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
   },
 }));
