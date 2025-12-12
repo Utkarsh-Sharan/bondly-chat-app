4cc8df25d5e4fde159817c72b3c16c1fc22d9c8b
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-12-12 11:10:53 +0530

    setup and test zustand

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,24 @@ import { Route, Routes } from "react-router";
 import ChatPage from "./pages/ChatPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import SignupPage from "./pages/SignupPage.jsx";
+import { useAuthStore } from "./store/useAuth.store.js";
 
 function App() {
+  const { authUser, isLoggedIn, login } = useAuthStore();
+
+  console.log("Auth user: ", authUser);
+  console.log("is logged in? : ", isLoggedIn);
+
   return (
     <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
       {/* GRID BG & GLOW SHAPES */}
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
       <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
       <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
+      <button className="btn btn-primary z-10" onClick={login}>
+        Click to login
+      </button>
+
       <Routes>
         <Route path="/" element={<ChatPage />} />
         <Route path="/signup" element={<SignupPage />} />
