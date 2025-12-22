99f05d5380101ec2259dbcbe1ffaf4485d3dd17d
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-12-22 10:00:50 +0530

    setup socket.js

---
+++ backend/src/lib/socket.js
@@ -0,0 +1,17 @@
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  },
+});
+
+io.use(socketAuth);
