655da1ee1caaeedefa64d1eab21ffa5d62334cbc
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-12-24 12:51:52 +0530

    setup for deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 const app = express();
+const __dirname = path.resolve();
 
 //Basic config
 app.use(express.json({ limit: "5mb" }));
@@ -11,14 +13,16 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 //CORS config
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: process.env.CORS_ORIGIN,
+      credentials: true,
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    }),
+  );
+}
 
 //Routes
 import authRouter from "./routes/auth.routes.js";
@@ -38,7 +42,16 @@ app.use((err, req, res, next) => {
     message: err.message || "Something went wrong",
     errors: err.errors || null,
     data: err.data || null,
+    ...(process.env.NODE_ENV !== "production" && { stack: err.stack }),
   });
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/*splat", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 export default app;
