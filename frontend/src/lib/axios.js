f70ce6b0364fd454957034db28f49f4b8522afa5
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2026-03-17 11:59:21 +0530

    add axios interceptor

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -7,3 +7,23 @@ export const axiosInstance = axios.create({
       : "/api",
   withCredentials: true,
 });
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if(error.response?.status === 401 && !originalRequest._retry){
+      originalRequest._retry = true;
+
+      try {
+        await axiosInstance.post("auth/refresh-token");
+        return axiosInstance(originalRequest);
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+)
